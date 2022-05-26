2045323154c4825828c17b7cfc00840fc8ac8d23
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-05-26 15:23:53 +0900

    Update

--- OutputAllData.py
+++ OutputAllData.py
@@ -2,6 +2,7 @@ from python.SelectKey import SelectKey
 
 with open("data/AllData.txt", "w", encoding="utf-8") as AllData:
     AllData.write("ClearCollect(Terminology,")
+    wordCount = 0
     for subject in ["Machine", "Electricity", "Information", "Construction", "Chemistry", "Others"]:
         with open("data/" + subject + "Data.txt", "r", encoding="utf-8") as TerminologyData:
             i = 0
@@ -10,4 +11,6 @@ with open("data/AllData.txt", "w", encoding="utf-8") as AllData:
                 AllData.write(SelectKey(i, subject.lower()) +
                               terminologyDataLine.replace("\\", ""))
                 i += 1
+            wordCount += i/6
     AllData.write(");")
+    print("単語数:" + str((int)(wordCount)))
